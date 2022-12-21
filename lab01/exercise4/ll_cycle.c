57e00847dce2546fd52fa94b099be7dd0848093b
wherenog <2418432948@qq.com>
2022-12-21 23:52:53 +0800

    lab01 done

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    const node *fast_ptr = head;
+    const node *slow_ptr = head;
+    if (head == NULL)
+        return 0;
+    if (fast_ptr->next->next == NULL) 
+        return 0;
+    while (fast_ptr != NULL && fast_ptr->next != NULL && fast_ptr->next->next != NULL) {
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+        if (fast_ptr == slow_ptr)
+            return 1;
+    }
 }
